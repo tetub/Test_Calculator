e8050e5fc95e324fc1f0b45a5d898a2a6075b7bc
tetub <dsarbnoname@gmail.com>
2023-07-23 17:06:28 +0300

    update stdout

---
+++ Project1/Project1/main.cpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "Header.h"
+#include <conio.h>
+#include "Calculator.h"
+//Разработайте на C++ консольную программу «Калькулятор», позволяющую объявлять переменные
+//и функции и выполнять арифметические операции над ними.
+
+
+
+
+
+void main() {
+	Calculator calculator;
+	calculator.handler();
+	_getch();
+}
